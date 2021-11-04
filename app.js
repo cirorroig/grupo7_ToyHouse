4397e739639bc5645151a136fdc4c8ff62528f57
CiroRodriguez <cirorodriguezroig@gmail.com>
2021-11-03 23:59:27 -0300

    Registro y carpetas

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hola Mundo")
+const path=require("path");
+const express=require("express");
+const app=express();
+
+app.listen(3000,()=>console.log("Funcionando en el puerto 3000"))
+
+app.get("/",(req,res)=> res.sendFile(path.join(__dirname,"/views/home.html")))
+app.get("/register",(req,res)=> res.sendFile(path.join(__dirname,"/views/register.html")))
+app.get("/login",(req,res)=> res.sendFile(path.join(__dirname,"/views/login.html")))
+app.get("/carrito",(req,res)=> res.sendFile(path.join(__dirname,"/views/carrito.html")))
+app.get("/producto",(req,res)=> res.sendFile(path.join(__dirname,"/views/producto.html")))
+
+
+app.use(express.static("public"))
