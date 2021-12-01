3e511cc5716de137e1293b1912110e3a0b07f952
CiroRodriguez <cirorodriguezroig@gmail.com>
2021-11-30 21:59:06 -0300

    cambios EJS

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const app=express();
 const indexRouter = require('./routes/index');
 
 
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname,"views"));
 app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname,"../public")))
 
 app.use('/', indexRouter);
 
--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,6 +1,6 @@
 const indexController = {
     index: (req,res) => {
-        res.render("index", {})
+        res.render("index")
     }
 }
 
